3a52eec232b5e67a2a78c9f4a7b9d723d566d6d8
jamill hallack <jnbezhal@gmail.com>
2024-11-11 23:59:05 +0400

    Add Optimized contracts files

--- contracts/contracts_optimized/ArraySum.sol
+++ contracts/contracts_optimized/ArraySum.sol
@@ -4,13 +4,11 @@ pragma solidity 0.8.15;
 contract OptimizedArraySum {
     // Do not modify this
     uint256[] array;
-
     // Do not modify this
     function setArray(uint256[] memory _array) external {
         require(_array.length <= 10, 'too long');
         array = _array;
     }
-
     // optimize this function
    function getArraySum() external view returns (uint256 sum ) {
         uint256 array_length = array.length;
